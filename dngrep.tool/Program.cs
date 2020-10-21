a0f2026b02352d1f6226731d60bb7f3c107282a1
psxvoid <pavel.sapehin@gmail.com>
2020-10-21 06:01:37 +0300

    Add an empty project for the global tool

---
+++ dngrep.tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dngrep.tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
