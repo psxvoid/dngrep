b3512ffc1c9d3801a495753783ddf49dc3c33dea
psxvoid <pavel.sapehin@gmail.com>
2020-10-21 07:18:17 +0300

    Add dotnet code analysis support

--- dngrep.tool/Program.cs
+++ dngrep.tool/Program.cs
@@ -4,6 +4,9 @@ namespace dngrep.tool
 {
     class Program
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Should be fixed during further development.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Should be fixed during further development.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Should be fixed during further development.")]
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
