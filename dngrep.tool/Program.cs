39dbf51e599b168da174702e8a03cdccd9599c9e
psxvoid <pavel.sapehin@gmail.com>
2020-10-23 09:18:18 +0300

    [POC] Display method names for solution/project
    Additional notes:
    - configure dngrep.tools as global tool
    - add required msbuild packages

--- dngrep.tool/Program.cs
+++ dngrep.tool/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using dngrep.core.CompilationExtensions;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace dngrep.tool
 {
@@ -7,9 +15,64 @@ namespace dngrep.tool
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Should be fixed during further development.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Should be fixed during further development.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Should be fixed during further development.")]
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string? currentDirectory = Directory.GetCurrentDirectory();
+
+            (SolutionAndProjectExplorer.PathKind kind, string path) =
+                SolutionAndProjectExplorer.GetSolutionOrProject(currentDirectory);
+
+            MSBuildLocator.RegisterDefaults();
+
+            using MSBuildWorkspace workspace = MSBuildWorkspace.Create();
+            workspace.LoadMetadataForReferencedProjects = true;
+            // workspace.WorkspaceFailed += Workspace_WorkspaceFailed;
+
+            IEnumerable<Project> projects;
+
+            switch (kind)
+            {
+                case SolutionAndProjectExplorer.PathKind.None:
+                    Console.WriteLine("No solution or project is found in the current directory");
+                    return;
+                case SolutionAndProjectExplorer.PathKind.Solution:
+                    {
+                        Solution solution = await workspace.OpenSolutionAsync(path).ConfigureAwait(false);
+                        projects = solution.Projects;
+                    }
+                    break;
+                case SolutionAndProjectExplorer.PathKind.Project:
+                    {
+                        Project project = await workspace.OpenProjectAsync(path).ConfigureAwait(false);
+                        projects = new[] { project };
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported path target.");
+            }
+
+            List<string> methodNames = new List<string>();
+
+            foreach (var proj in projects)
+            {
+                Compilation? compilation = await proj.GetCompilationAsync().ConfigureAwait(false);
+
+                if (compilation != null && compilation is CSharpCompilation cSharpCompilation)
+                {
+                    IReadOnlyCollection<string>? projectMethods = cSharpCompilation.GetMethodNames();
+
+                    methodNames.AddRange(projectMethods);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Only C# projects are supported.");
+                }
+            }
+
+            foreach (var methodName in methodNames)
+            {
+                Console.WriteLine(methodName);
+            }
         }
     }
 }
---
+++ dngrep.tool/SolutionAndProjectExplorer.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace dngrep.tool
+{
+    public static class SolutionAndProjectExplorer
+    {
+        public enum PathKind
+        {
+            None,
+            Solution,
+            Project
+        }
+
+        public static (PathKind kind, string path) GetSolutionOrProject(string path)
+        {
+            string[] solutions = Directory.GetFiles(path, "*.sln");
+            string[]? projects;
+
+            if (solutions.Length > 0)
+            {
+                return (PathKind.Solution, solutions[0]);
+            }
+            else
+            {
+                projects = Directory.GetFiles(path, "*.csproj");
+            }
+
+            if (projects != null && projects.Length > 0)
+            {
+                return (PathKind.Project, projects[0]);
+            }
+
+            return (PathKind.None, string.Empty);
+        }
+    }
+}
