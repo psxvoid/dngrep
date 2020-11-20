54d4c91b4062e85b4eec9cb89d76b982cd39956a
psxvoid <pavel.sapehin@gmail.com>
2020-11-20 09:45:39 +0300

    Allow single char span to have zero-length

--- dngrep.core.xunit/Extensions/SourceTextExtensions/SourceTextExtensionsTests.cs
+++ dngrep.core.xunit/Extensions/SourceTextExtensions/SourceTextExtensionsTests.cs
@@ -7,69 +7,128 @@ using Xunit;
 
 namespace dngrep.core.xunit.Extensions.SourceTextExtensions
 {
-    public class SourceTextExtensionsTests
+    public static class SourceTextExtensionsTests
     {
-        private const string ClassString = "class Person { int age; }";
-
-        private readonly SyntaxTree tree;
-
-        public SourceTextExtensionsTests()
+        public class SingleLineClassAndField
         {
-            this.tree = CSharpSyntaxTree.ParseText(ClassString);
-        }
-        
-        [Fact]
-        public void GetSingleCharSpan_Null_Throws()
-        {
-            Assert.Throws<ArgumentNullException>(
+            private const string ClassString = "class Person { int age; }";
+
+            private readonly SyntaxTree tree;
+
+            public SingleLineClassAndField()
+            {
+                this.tree = CSharpSyntaxTree.ParseText(ClassString);
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_Null_Throws()
+            {
+                Assert.Throws<ArgumentNullException>(
 #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
                 () => ((SourceText)null).GetSingleCharSpan(0, 0));
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            }
+
+            [Theory]
+            [InlineData(0, -1)]
+            [InlineData(-1, 0)]
+            [InlineData(1, 0)]
+            public void GetSingleCharSpan_OutOfRange_ThrowsOutOfRange(int line, int charPos)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => this.tree.GetText().GetSingleCharSpan(line, charPos));
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_ZeroZero_EntireLine()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 0);
+
+                Assert.Equal(new TextSpan(0, 1), result);
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_ZeroZero_EntireLineString()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 0);
+
+                Assert.Equal("c", this.tree.GetText().ToString(result));
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_ZeroSix_ShouldReturnSpan()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 6);
+
+                Assert.Equal(new TextSpan(6, 1), result);
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_ZeroSix_ShouldReturnSpanString()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 6);
+
+                Assert.Equal("P", this.tree.GetText().ToString(result));
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_GreaterOrEqualToLength_ShouldReturnLastChar()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, ClassString.Length);
+
+                Assert.Equal(new TextSpan(24, 1), result);
+            }
+
+            [Fact]
+            public void GetSingleCharSpan_GreaterOrEqualToLength_ShouldReturnLastCharString()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, ClassString.Length);
+
+                Assert.Equal("}", this.tree.GetText().ToString(result));
+            }
         }
 
-        [Theory]
-        [InlineData(0, -1)]
-        [InlineData(0, 26)]
-        [InlineData(-1, 0)]
-        [InlineData(1, 0)]
-        public void GetSingleCharSpan_OutOfRange_ThrowsOutOfRange(int line, int charPos)
+        public class EmptyLine
         {
-            Assert.Throws<ArgumentOutOfRangeException>(
-                () => this.tree.GetText().GetSingleCharSpan(line, charPos));
-        }
+            private const string ClassString = "";
 
-        [Fact]
-        public void GetSingleCharSpan_ZeroZero_EntireLine()
-        {
-            TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 0);
+            private readonly SyntaxTree tree;
 
-            Assert.Equal(new TextSpan(0, 1), result);
-        }
+            public EmptyLine()
+            {
+                this.tree = CSharpSyntaxTree.ParseText(ClassString);
+            }
 
-        [Fact]
-        public void GetSingleCharSpan_ZeroZero_EntireLineString()
-        {
-            TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 0);
+            [Fact]
+            public void GetSingleCharSpan_Null_Throws()
+            {
+                Assert.Throws<ArgumentNullException>(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                () => ((SourceText)null).GetSingleCharSpan(0, 0));
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            }
 
-            Assert.Equal("c", this.tree.GetText().ToString(result));
-        }
+            [Theory]
+            [InlineData(0, -1)]
+            [InlineData(-1, 0)]
+            [InlineData(1, 0)]
+            public void GetSingleCharSpan_OutOfRange_ThrowsOutOfRange(int line, int charPos)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => this.tree.GetText().GetSingleCharSpan(line, charPos));
+            }
 
-        [Fact]
-        public void GetSingleCharSpan_ZeroSix_ShouldReturnSpan()
-        {
-            TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 6);
+            [Fact]
+            public void GetSingleCharSpan_GreaterOrEqualToLength_ShouldReturnZeroLengthSpan()
+            {
+                TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 1);
 
-            Assert.Equal(new TextSpan(6, 1), result);
-        }
-
-        [Fact]
-        public void GetSingleCharSpan_ZeroSix_ShouldReturnSpanString()
-        {
-            TextSpan result = this.tree.GetText().GetSingleCharSpan(0, 6);
-
-            Assert.Equal("P", this.tree.GetText().ToString(result));
+                Assert.Equal(new TextSpan(0, 0), result);
+            }
         }
     }
 }
--- dngrep.core/Extensions/SourceTextExtensions/SourceTextExtensions.cs
+++ dngrep.core/Extensions/SourceTextExtensions/SourceTextExtensions.cs
@@ -5,6 +5,26 @@ namespace dngrep.core.Extensions.SourceTextExtensions
 {
     public static class SourceTextExtensions
     {
+        /// <summary>
+        /// Get <see cref="TextSpan"/> of length one (or zero for an empty line)
+        /// for a specified line and char position in a source text. For example,
+        /// it can be used to get calculate a cursor location in the source text.
+        /// </summary>
+        /// <param name="source">
+        /// The source text where the line is belongs.
+        /// </param>
+        /// <param name="line">
+        /// Zero-based line number in the source text for which a single character's
+        /// span should be retrieved.
+        /// </param>
+        /// <param name="charStart">
+        /// Zero-based character number in the source text at the specified line for
+        /// which a span should be retrieved.
+        /// </param>
+        /// <returns>
+        /// The span of a single character located at the specified line and position.
+        /// The span contains exact location of the character.
+        /// </returns>
         public static TextSpan GetSingleCharSpan(this SourceText source, int line, int charStart)
         {
             _ = source ?? throw new ArgumentNullException(nameof(source));
@@ -18,14 +38,27 @@ namespace dngrep.core.Extensions.SourceTextExtensions
 
             TextSpan lineSpan = source.Lines[line].Span;
 
-            if (charStart < 0 || charStart >= lineSpan.Length)
+            if (charStart < 0)
             {
                 throw new ArgumentOutOfRangeException(
                     nameof(line),
                     "Specified char position is out of range from the source text.");
             }
 
-            return new TextSpan(lineSpan.Start + charStart, 1);
+            if (charStart >= lineSpan.Length)
+            {
+                charStart = lineSpan.Length - 1;
+            }
+
+            int spanLength = 1;
+
+            if (lineSpan.Length == 0)
+            {
+                charStart = 0;
+                spanLength = 0;
+            }
+
+            return new TextSpan(lineSpan.Start + charStart, spanLength);
         }
     }
 }
